c3734ecd03a4c9a6b6d0b6a73d174befa1aaf702
bonelessnugget <yszeedyt99@gmail.com>
2022-04-12 13:04:36 +0200

    Mortadela

---
+++ daotsJson.js
@@ -0,0 +1,43 @@
+// Replace ./data.json with your JSON feed
+fetch('./prediccionesZaragoza.json')
+  .then((response) => {
+    return response.json()
+  })
+  .then((datos) => {
+      for (let dato of datos) {
+        console.log(dato);
+      }
+    document.getElementById("dia1").addEventListener("click", function(){
+       
+        for (var i = 0; i<datos.length; i++){
+            document.getElementById("txt-temperatura").innerHTML = (datos[i].prediccion.dia[0].temperatura.maxima + datos[i].prediccion.dia[0].temperatura.minima) / 2 + " º";
+
+        }
+
+        for (var i = 0; i<datos.viento.length; i++){
+            console.log(datos[i].prediccion.dia[0].viento[i].velocidad);
+        }
+    });
+    
+
+  })
+  .catch((err) => {
+    // Do something for an error here
+  })
+
+function abrirCalendario(){
+    var modal = document.getElementById("calendario-popup");
+    var btn = document.getElementById("calendario");
+    btn.onclick = function() {
+        modal.style.display = "block";
+        modal.style.alignSelf = "center";
+        modal.style.zIndex = 1000;
+        modal.style.position = "absolute";
+        modal.style.width = "40%";
+    }
+}
+
+function cerrarCalendario(){
+    var modal = document.getElementById("calendario-popup");
+    modal.style.display = "none";
+}
